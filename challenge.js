54689f193591c82ce4986a2dc73cc4dd97061908
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-10-26 01:56:48 +0800

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,126 +1,137 @@
 var SNAKE_BLOCK_SIZE = 20;
 
-var s=0
+var s = 0
 
 var snakeHead = {
-  x: 100,
-  y: 100
+	x: 100,
+	y: 100
 }
 var snakeSegments = [];
 
 var snakeDirection = "right"; // or "down", etc.
 
 var foodLocation = {
-  x: 200,
-  y: 200
+	x: 200,
+	y: 200
 }
 
 function setup() {
-  createCanvas(400, 400);
-  frameRate(2);
-  
-  // add 2 segments
-  extendSnake();
-  extendSnake();
-  
-  background(0);
-  drawSegment(snakeHead);
+	createCanvas(400, 400);
+	frameRate(2);
+
+	// add 2 segments
+	extendSnake();
+	extendSnake();
+
+	background(0);
+	drawSegment(snakeHead);
 
 }
 
 function draw() {
-  background(0);
+	background(0);
 
-  moveSnake();
-  checkFoodReached();
-  hitboundery(snakeHead);
+	moveSnake();
+	checkFoodReached();
+	hitboundery(snakeHead);
 
-  // draw snake head
-  drawSegment(snakeHead);
-    
-  // draw snake body
-  snakeSegments.forEach(drawSegment);
-  
-  // draw food location
-  ellipse(foodLocation.x, foodLocation.y, SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE);
+	// draw snake head
+	drawSegment(snakeHead);
+
+	// draw snake body
+	snakeSegments.forEach(drawSegment);
+
+	// draw food location
+	ellipse(foodLocation.x, foodLocation.y, SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE);
 
 	snakeHitbody(snakeHead);
 }
 
 function drawSegment(segment) {
-  noStroke();
-  fill(255);
-  rectMode(CENTER);
-  rect(segment.x, segment.y, SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE);
-}  
+	noStroke();
+	fill(255);
+	rectMode(CENTER);
+	rect(segment.x, segment.y, SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE);
+}
 
 function moveSnake() {
-  // add snakeHead to front of segments array
-  snakeSegments.unshift({x: snakeHead.x, y: snakeHead.y});
-  // remove last element of the segments array
-  snakeSegments.pop();
-  
-  if (snakeDirection == "up") {
-    snakeHead.y = snakeHead.y - SNAKE_BLOCK_SIZE;
-  } else if (snakeDirection == "down") {
-    snakeHead.y = snakeHead.y + SNAKE_BLOCK_SIZE;
-  } else if (snakeDirection == "left") {
-    snakeHead.x = snakeHead.x - SNAKE_BLOCK_SIZE;
-  } else if (snakeDirection == "right") {
-    snakeHead.x = snakeHead.x + SNAKE_BLOCK_SIZE;
-  }
+	// add snakeHead to front of segments array
+	snakeSegments.unshift({
+		x: snakeHead.x,
+		y: snakeHead.y
+	});
+	// remove last element of the segments array
+	snakeSegments.pop();
+
+	if (snakeDirection == "up") {
+		snakeHead.y = snakeHead.y - SNAKE_BLOCK_SIZE;
+	} else if (snakeDirection == "down") {
+		snakeHead.y = snakeHead.y + SNAKE_BLOCK_SIZE;
+	} else if (snakeDirection == "left") {
+		snakeHead.x = snakeHead.x - SNAKE_BLOCK_SIZE;
+	} else if (snakeDirection == "right") {
+		snakeHead.x = snakeHead.x + SNAKE_BLOCK_SIZE;
+	}
 }
 
 function keyPressed() {
-  if (keyCode == UP_ARROW) {
-    snakeDirection = "up";
-  } else if (keyCode == DOWN_ARROW) {
-    snakeDirection = "down";
-  } else if (keyCode == RIGHT_ARROW) {
-    snakeDirection = "right";
-  } else if (keyCode == LEFT_ARROW) {
-    snakeDirection = "left";
-  }
+	if (keyCode == UP_ARROW) {
+		snakeDirection = "up";
+	} else if (keyCode == DOWN_ARROW) {
+		snakeDirection = "down";
+	} else if (keyCode == RIGHT_ARROW) {
+		snakeDirection = "right";
+	} else if (keyCode == LEFT_ARROW) {
+		snakeDirection = "left";
+	}
 }
 
 function extendSnake() {
-  // get the last segment -- or the head segment if there aren't any segments
-  var lastSegment = snakeSegments[snakeSegments.length-1] || snakeHead;
-  // duplicate last segment
-  snakeSegments.push({x: lastSegment.x, y: lastSegment.y}); 
+	// get the last segment -- or the head segment if there aren't any segments
+	var lastSegment = snakeSegments[snakeSegments.length - 1] || snakeHead;
+	// duplicate last segment
+	snakeSegments.push({
+		x: lastSegment.x,
+		y: lastSegment.y
+	});
 }
 
 function checkFoodReached() {
-	
 
-	
-  if (dist(snakeHead.x, snakeHead.y, foodLocation.x, foodLocation.y) == 0) {
-    foodLocation.x = SNAKE_BLOCK_SIZE * floor(random(width / SNAKE_BLOCK_SIZE));
-    foodLocation.y = SNAKE_BLOCK_SIZE * floor(random(height / SNAKE_BLOCK_SIZE));
-		
-		s=s+1
+
+
+	if (dist(snakeHead.x, snakeHead.y, foodLocation.x, foodLocation.y) == 0) {
+		foodLocation.x = SNAKE_BLOCK_SIZE * floor(random(width / SNAKE_BLOCK_SIZE));
+		foodLocation.y = SNAKE_BLOCK_SIZE * floor(random(height / SNAKE_BLOCK_SIZE));
+
+		s = s + 1
 		textSize(32);
-		text("score: "+s, 10, 30);
-		
-    // make the snake longer!
-    extendSnake();		
+		text("score: " + s, 10, 30);
 
-  }
+		// make the snake longer!
+		extendSnake();
+
+	}
 }
 
-	function hitboundery(){
-	if(snakeHead.x>=400||snakeHead.x<=0||snakeHead.y>=400||snakeHead.y<=0){
-		 textSize(13);
-		text("you exceed the boundery!!",10,30)
-		 noLoop();
-		
+function hitboundery() {
+	if (snakeHead.x >= 400 || snakeHead.x <= 0 || snakeHead.y >= 400 || snakeHead.y <= 0) {
+		textSize(13);
+		text("you exceed the boundery!!", 10, 30)
+		noLoop();
+
+	}
 }
-}
-	function snakeHitbody(){
-for (var index = 0; index < snakeSegments.length; index += 1) {
-  // check for hit using snakeSegments[index].x and snakeSegments[index].y
-			noLoop();
-}
-		print("what's index", index);
-		print("what's snakeSegments.length", snakeSegments.length);
+
+function snakeHitbody() {
+	for (var index = 0; index < snakeSegments.length; index += 1) {
+		if (snakeSegments[index].x==snakeHead.x &&  snakeSegments[index].y==snakeHead.y ) {
+		noLoop();
 		}
+}
+	// print("what's snakeSegments[index]:  ", snakeSegments[index].x);
+	// 	print("snake head:  ", snakeHead.x);
+
+	
+	// print("what's snakeSegments.length", snakeSegments.length);
+}
